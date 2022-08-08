eaf62523a6c0655e5cc8cb8122063b64cdfc79f1
shyfullahsadi <shyfullah.sadi@gmail.com>
2022-08-08 06:42:08 +0100

    array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-const cars = [
-    'Fielder',
-    'Toyota',
-    'volvo'
-]
 
-let arrayValuePrint = document.getElementById('demo');
 
-arrayValuePrint.innerHTML = cars;
+const color =[];
+
+color[0] = "red";
+
+color[1] = "green";
+
+color[2] = "blue";
+
+color[3] = "orange";
+
+document.getElementById('demo').innerHTML = color
